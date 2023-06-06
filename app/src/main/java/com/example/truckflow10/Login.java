9f5ca545b7f396daa3f795e9f160c0eed6d8b69e
Aman <amanmishra4191@gmail.com>
2023-06-06 16:30:50 -0700

    initial commit

---
+++ app/src/main/java/com/example/truckflow10/Login.java
@@ -0,0 +1,29 @@
+package com.example.truckflow10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+     Button callSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+        callSignUp = findViewById(R.id.signUp_screen);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
