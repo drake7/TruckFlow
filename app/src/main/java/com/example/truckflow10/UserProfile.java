5c6bf84402e268a72a092ce2a5723aeeca41027d
Aman <amanmishra4191@gmail.com>
2023-06-08 23:43:28 -0700

    User Profile with fixes

--- app/src/main/java/com/example/truckflow10/UserProfile.java
+++ app/src/main/java/com/example/truckflow10/UserProfile.java
@@ -9,6 +9,6 @@ public class UserProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile2);
+        setContentView(R.layout.activity_user_profile);
     }
 }
