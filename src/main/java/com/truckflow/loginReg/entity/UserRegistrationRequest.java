c6b27fea708b7d9ac2428ee513f3ad1233fb230c
RajatKadian <rajat.kadian007@yahoo.com>
2023-06-28 14:47:18 -0700

    changes

--- src/main/java/com/truckflow/loginReg/entity/UserRegistrationRequest.java
+++ src/main/java/com/truckflow/loginReg/entity/UserRegistrationRequest.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class UserRegistrationRequest {
 
-    private String user_id;
+//    private String user_id;
     private String name;
     private String email;
     private String Password;
